34e89381d4754bfc72a5459b6702282fca252fbb
BitPatty <matteias.collet@bluewin.ch>
2018-01-31 23:10:50 +0100

    fastcode interface

--- gctGenerator.js
+++ gctGenerator.js
@@ -27,12 +27,12 @@ function parseXML(name) {
         var desc = xmlData[i].getElementsByTagName("title")[0].textContent;
         var t = document.createTextNode(desc);
         li.appendChild(t);
-        li.setAttribute("data-codename", xmlData[i].getElementsByTagName("title")[0].textContent);
-        li.setAttribute("data-codeauthor", xmlData[i].getElementsByTagName("author")[0].textContent);
-        li.setAttribute("data-codedesc", xmlData[i].getElementsByTagName("description")[0].textContent);
-        li.setAttribute("data-codeversion", xmlData[i].getElementsByTagName("version")[0].textContent);
-        li.setAttribute("data-codedate", xmlData[i].getElementsByTagName("date")[0].textContent);
-        li.setAttribute("data-codesrc", xmlData[i].getElementsByTagName("source")[0].textContent.replace(/[\s\n\r\t]+/gm, ""));
+        li.setAttribute("data-codename", btoa(xmlData[i].getElementsByTagName("title")[0].textContent));
+        li.setAttribute("data-codeauthor", btoa(xmlData[i].getElementsByTagName("author")[0].textContent));
+        li.setAttribute("data-codedesc", btoa(xmlData[i].getElementsByTagName("description")[0].textContent));
+        li.setAttribute("data-codeversion", btoa(xmlData[i].getElementsByTagName("version")[0].textContent));
+        li.setAttribute("data-codedate", btoa(xmlData[i].getElementsByTagName("date")[0].textContent));
+        li.setAttribute("data-codesrc", btoa(xmlData[i].getElementsByTagName("source")[0].textContent.replace(/[\s\n\r\t]+/gm, "")));
         li.setAttribute("onmouseover", "updateDescription(this)");
         document.getElementById("checklist").appendChild(li);
       }
@@ -46,6 +46,18 @@ function parseXML(name) {
 
   xml.open("GET", file);
   xml.send();
+}
+
+function updateFastCode(name) {
+   var xml = new XMLHttpRequest();
+   var file = "codes/fast/" + name + ".json";
+   xml.onreadystatechange = function() {
+      if (this.status == 200 && this.readyState == 4) {
+         document.getElementById("route_levels").setAttribute("data-json", btoa(xml.responseText));
+      }
+   }
+   xml.open("GET",file);
+   xml.send();
 }
 
 function downloadFile(data, filename) {
@@ -76,9 +88,16 @@ function generateGCT() {
    var valueSelected = false;
    for (var i = 0; i < codeList.length; i++) {
       if (codeList[i].className === "checked") {
-         data += codeList[i].getAttribute("data-codesrc");
+         data += atob(codeList[i].getAttribute("data-codesrc"));
          valueSelected = true;
       }
+   }
+   
+   var fastcode = getFastCode();
+   
+   if(fastcode !== false) { 
+      data += fastcode;
+      valueSelected = true;
    }
 
    if (valueSelected) {
@@ -109,10 +128,21 @@ function generateTXT(s) {
          data += "\r\n";
          if (s.id === "gcmdownloadbutton") data += "\r\n";
          else data += "$";
-         data += codeList[i].getAttribute("data-codename") + " (" + codeList[i].getAttribute("data-codedate") + ") [" + codeList[i].getAttribute("data-codeauthor") + "]\r\n";
-         data += (codeList[i].getAttribute("data-codesrc").match(/.{8}/g).join(" ")).replace(/(.{17})./g, "$1\r\n");
+         data += atob(codeList[i].getAttribute("data-codename")) + " (" + atob(codeList[i].getAttribute("data-codedate")) + ") [" + atob(codeList[i].getAttribute("data-codeauthor")) + "]\r\n";
+         data += (atob(codeList[i].getAttribute("data-codesrc")).match(/.{8}/g).join(" ")).replace(/(.{17})./g, "$1\r\n");
          valueSelected = true;
       }
+   }
+   
+   var fastcode = getFastCode();
+   
+   if(fastcode !== false) {
+      data += "\r\n";
+      if (s.id === "gcmdownloadbutton") data += "\r\n";
+      else data += "$";
+      data += "Stage list loader [Noki Doki]\r\n";
+      data += (fastcode.match(/.{8}/g).join(" ")).replace(/(.{17})./g, "$1\r\n");
+      valueSelected = true;
    }
 
    if (valueSelected)
@@ -128,6 +158,7 @@ function updateCodelist() {
    document.getElementById("checklist").innerHTML = "";
    var gameVersion = document.getElementById("gameversion").value;
    parseXML(gameVersion);
+   updateFastCode(gameVersion);
    document.getElementById("home").style.display = "none";
    document.getElementById("left").style.visibility = "visible";
    document.getElementById("center").style.visibility = "visible";
@@ -136,11 +167,11 @@ function updateCodelist() {
 
 function updateDescription(s) {
    document.getElementById("descriptionbox").innerHTML = "<h2>" +
-      s.getAttribute("data-codename") + "</h2><p style=\"margin:0\"><i>Author(s): " +
-      s.getAttribute("data-codeauthor") + "</i></p><p style=\"margin:0\"><i>Version: " +
-      s.getAttribute("data-codeversion") + " (" +
-      s.getAttribute("data-codedate") + ")</i></p>" + "<br /><h4>Description:</h4><p>" +
-      s.getAttribute("data-codedesc") + "</p>";
+      atob(s.getAttribute("data-codename")) + "</h2><p style=\"margin:0\"><i>Author(s): " +
+      atob(s.getAttribute("data-codeauthor")) + "</i></p><p style=\"margin:0\"><i>Version: " +
+      atob(s.getAttribute("data-codeversion")) + " (" +
+      atob(s.getAttribute("data-codedate")) + ")</i></p>" + "<br /><h4>Description:</h4><p>" +
+      atob(s.getAttribute("data-codedesc")) + "</p>";
 }
 
 function setButtonDescription(s) {
@@ -179,17 +210,19 @@ function updateChangelog() {
 
 /****************************
 *
-*  Fastcode UI
+*  Fastcode, https://github.com/QbeRoot/fastcodes/blob/master/script.js
 *
 ****************************/
 
 'strict mode'
 const levels = document.querySelector("#route_levels");
 const template = levels.lastElementChild;
+template.ondragstart = function() { return false; };
 
 function appendLevel(code) {
    const clone = template.cloneNode(true);
    clone.draggable = true;
+   selSetHandlers(clone);
    clone.querySelector("select").value = code;
    levels.insertBefore(clone, template);
 }
@@ -211,25 +244,6 @@ levels.addEventListener("click", function ({target: t}) {
    if (t.tagName.toUpperCase() === "BUTTON") levels.removeChild(t.parentNode);
 })
 
-{
-   let dragging
-   
-   levels.addEventListener('dragstart', function ({target: t, dataTransfer: d}) {
-      if (t.nodeName.toUpperCase() !== "LI" || t === template) {
-         d.clearData()
-      } else {
-         dragging = t
-         d.setData('text/plain', t.querySelector('select').value)
-      }
-   })
-   
-   levels.addEventListener('dragover', function ({target: t, dataTransfer: d}) {
-      if (t !== dragging && t.nodeName.toUpperCase() === "LI" && d.items.length > 0) {
-         levels.insertBefore(dragging, t)
-      }
-   })
-}
-
 document.querySelector("#route_presets").addEventListener("change", function ({currentTarget: t}) {
    if (levels.childElementCount <= 1 || confirm("Loading a preset will erase your current list. Continue?")) {
       clearLevels();
@@ -243,8 +257,138 @@ document.querySelector("#route_clear").addEventListener("click", function () {
    confirm("Do you really want to clear the list?") && clearLevels();
 })
 
-/******************
-*
-* Interface (TODO)
-*
-*******************/
+{
+   let selection;
+   
+   function selDragStart(e) {
+      selection = this;
+      e.dataTransfer.effectAllowed = "move";
+      e.dataTransfer.setData("text/html", this.querySelector("select").value);
+      
+      this.classList.add("dragelement");
+   }
+   
+   function selDragOver(e) {
+      if (e.preventDefault) {
+         e.preventDefault();
+      }
+      
+      this.classList.add("dragover");
+      
+      e.dataTransfer.dropEffect = "move";
+      
+      return false;
+   }
+   
+   function selDragLeave() {
+      this.classList.remove("dragover");
+   }
+   
+   function selDragDrop(e) {
+      if (e.stopPropagation) {
+         e.stopPropagation();
+      }
+      
+      if (selection != this) {
+         this.parentNode.removeChild(selection);
+         this.insertAdjacentHTML("afterend", template.outerHTML);
+         this.nextSibling.querySelector("select").value = e.dataTransfer.getData('text/html');
+         this.nextSibling.draggable = true;
+         selSetHandlers(this.nextSibling);
+      }
+      
+      this.classList.remove("dragover");
+      return false;
+   }
+   
+   function selDragEnd() {
+      this.classList.remove("dragelement");
+   }
+   
+   function selSetHandlers(elem) {
+      elem.addEventListener('dragstart', selDragStart, false);
+      elem.addEventListener('dragover', selDragOver, false);
+      elem.addEventListener('dragleave', selDragLeave, false);
+      elem.addEventListener('drop', selDragDrop, false);
+      elem.addEventListener('dragend', selDragEnd, false);
+   }
+}
+
+//Interface 
+function getFastCode() {
+   
+   const levelCodes = Array.prototype.map.call(levels.querySelectorAll("select"), s => s.value);
+   
+   levelCodes.pop();
+   
+   if (!(document.getElementById("usefastcode").checked) || levelCodes.length === 1) return false;  
+   
+   let game = JSON.parse(atob(document.getElementById("route_levels").getAttribute("data-json")));
+   const randomize = (document.getElementById("route_random").value === "yes");
+   const levelWords = Math.ceil(levelCodes.length / 2);
+   const asm = [];
+   asm.push("48" + ("00000" + (levelWords + 1 << 2 | 1).toString(16).toUpperCase()).slice(-6)); // bl to the code
+   for (let i = 0; i < levelWords; ++i) {
+      asm.push(levelCodes[2 * i] + (levelCodes[2 * i + 1] || "0000"));
+   }
+   if (!randomize) {
+      asm.push("3C80817F"); // lis r4, 0x817F
+   }
+   
+   //Timer compatibility
+   asm.push("38000000"); // li r0, 0
+   asm.push("9004010C"); // stw r0, 0x010C(r4)
+   asm.push("38000001"); // li r0, 1
+   asm.push("98040101"); // stb r0, 0x0101(r4)
+   
+   asm.push("881F0012"); // lbz r0, 0x12(r31)
+   
+   if (!randomize) {
+      asm.push("2C00000F"); // cmpwi r0, 15
+      asm.push("40820010"); // bne- 0x10
+      asm.push("38000000"); // li r0, 0
+      asm.push("90040000"); // stw r0, 0(r4)
+      asm.push("4800002C"); // b 0x2C
+   }
+   
+   asm.push("2C000001"); // cmpwi r0, 1
+   
+   if (randomize) {
+      asm.push("41810030"); // bgt- 0x30
+      asm.push("7C6C42E6"); // mftbl r3
+      asm.push("3880" + ("000" + (levelCodes.length * 2).toString(16).toUpperCase()).slice(-4)); // li r4, length
+      asm.push("7C032396"); // divwu r0, r3, r4
+      asm.push("7C0021D6"); // mullw r0, r0, r4
+      asm.push("7C601850"); // sub r3, r3, r0
+      asm.push("5463003C"); // rlwinm r3, r3, 0, 0, 30
+   } else {
+      asm.push("41810024"); // bgt- 0x24
+      asm.push("80640000"); // lwz r3, 0(r4)
+      asm.push("38030002"); // addi r0, r3, 2
+      asm.push("90040000"); // stw r0, 0(r4)
+   }
+   
+   asm.push("7C8802A6"); // mflr r4
+   asm.push("7C641A2E"); // lhzx r3, r4, r3
+   asm.push("B07F0012"); // sth r3, 0x12(r31)
+   asm.push("808D" + game.fmOffset); // lwz r4, TFlagManager::smInstance
+   asm.push("986400DF"); // stb r3, 0xDF(r4)
+   asm.push("807F0020"); // lwz r3, 0x20(r31)
+   
+   if (asm.length % 2 === 0) {
+      asm.push("60000000"); // nop
+   }
+   asm.push("00000000");
+   
+   const geckoLines = asm.length / 2;
+   let gecko = "C2" + game.injectAddr + " " + ("0000000" + geckoLines.toString(16).toUpperCase()).slice(-8) + "\r\n";
+   for (let i = 0; i < geckoLines; ++i) {
+      gecko += asm[2 * i] + " " + asm[2 * i + 1] + "\r\n";
+   }
+   
+   let codes = gecko + 
+      game.notext[document.getElementById("route_notext").value] +
+      game.nofmvs[document.getElementById("route_nofmvs").value];
+   
+   return codes.replace(/[^0-9A-F]/g, '');
+}
