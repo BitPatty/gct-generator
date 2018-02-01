170beaccc69442344bc0411122dc22cf4fb9c9ac
BitPatty <matteias.collet@bluewin.ch>
2018-02-01 07:12:51 +0100

    cleanup main site

--- gctGenerator.js
+++ gctGenerator.js
@@ -92,10 +92,10 @@ function generateGCT() {
          valueSelected = true;
       }
    }
-   
+
    var fastcode = getFastCode();
-   
-   if(fastcode !== false) { 
+
+   if(fastcode !== false) {
       data += fastcode;
       valueSelected = true;
    }
@@ -114,32 +114,35 @@ function generateGCT() {
    }
 }
 
-function generateTXT(s) {
+function generateTXT() {
+
+   var dolphin = (document.getElementById("downloadformat").value === "ini");
+
    if (document.getElementById("gameversion").value === "Choose Version") {
       alert("Select the game version!");
       return;
    }
-   if (s.id === "dolphindownloadbutton") var data = "Paste the following on top of your games .ini file:\r\n[Gecko]";
+   if (dolphin) var data = "Paste the following on top of your games .ini file:\r\n[Gecko]";
    else var data = document.getElementById("gameversion").value.replace("GMSJ0A","GMSJ01") + "\r\nSuper Mario Sunshine";
    var codeList = document.getElementById("checklist").getElementsByTagName("li");
    var valueSelected = false;
    for (var i = 0; i < codeList.length; i++) {
       if (codeList[i].className === "checked") {
          data += "\r\n";
-         if (s.id === "gcmdownloadbutton") data += "\r\n";
-         else data += "$";
+         if (dolphin) data += "$";
+         else data += "\r\n";
          data += atob(codeList[i].getAttribute("data-codename")) + " (" + atob(codeList[i].getAttribute("data-codedate")) + ") [" + atob(codeList[i].getAttribute("data-codeauthor")) + "]\r\n";
          data += (atob(codeList[i].getAttribute("data-codesrc")).match(/.{8}/g).join(" ")).replace(/(.{17})./g, "$1\r\n");
          valueSelected = true;
       }
    }
-   
+
    var fastcode = getFastCode();
-   
+
    if(fastcode !== false) {
       data += "\r\n";
-      if (s.id === "gcmdownloadbutton") data += "\r\n";
-      else data += "$";
+      if (dolphin) data += "$";
+      else data += "\r\n";
       data += "Stage list loader [Noki Doki]\r\n";
       data += (fastcode.match(/.{8}/g).join(" ")).replace(/(.{17})./g, "$1\r\n");
       valueSelected = true;
@@ -148,6 +151,11 @@ function generateTXT(s) {
    if (valueSelected)
    downloadFile(data, document.getElementById("gameversion").value + ".txt");
    else alert("No cheat(s) selected!");
+}
+
+function downloadCodes() {
+   if (document.getElementById("downloadformat").value === "gct") generateGCT();
+   else generateTXT();
 }
 
 function updateCodelist() {
@@ -174,29 +182,34 @@ function updateDescription(s) {
       atob(s.getAttribute("data-codedesc")) + "</p>";
 }
 
-function setButtonDescription(s) {
-   if (s.id === "downloadbutton")
-      document.getElementById("descriptionbox").innerHTML = "<h2>Download GCT</h2><p>Download the cheats in the GCT format for use with Nintendont.</p>";
-   else if (s.id === "dolphindownloadbutton")
-      document.getElementById("descriptionbox").innerHTML = "<h2>Download for Dolphin</h2><p>Download a textfile containing the formatted codes for use with Dolphin. Copy the contents of the file on top of your games .ini file.</p><p>You can open the .ini file by right clicking the game in Dolphin. In the context menu select \"<i>Properties</i>\" and then \"<i>Edit configuration</i>\".</p>";
-   else
-      document.getElementById("descriptionbox").innerHTML = "<h2>Download for Gecko Cheat Manager</h2><p>Download the cheats in a textfile formatted for use with the <a href=\"http://wiibrew.org/wiki/CheatManager\">Gecko Cheat Manager</a>. Place the txt file in SD:/txtcodes/.</p><p>A zip archive containing pregenerated txt files with all available codes on this site can be downloaded <a href=\"files/GCMCodes.zip\">here</a>.";
-}
-
-function setDropdownDescription(s) {
+function updateUIDescription(s) {
    if (s.id === "route_notext")
       document.getElementById("descriptionbox").innerHTML = "<h2>Remove Dialogue</h2><p>Replaces all Dialogue with \"!!!\". 'Always' and 'Not in Pianta 5' will override the dialogue skip from the DPad Functions.</p>";
    else if (s.id === "route_nofmvs")
       document.getElementById("descriptionbox").innerHTML = "<h2>Skippable Cutscenes</h2><p>Makes FMVs Skippable. 'Always' has the same effect as the 'FMV Skips' code. Also, having 'FMV Skips' enabled will override 'Not in Pinna 1' - so don't use both simultaneously.</p>";
    else if (s.id === "route_random")
       document.getElementById("descriptionbox").innerHTML = "<h2>Random Level Order</h2><p>Randomizes what level is loaded next - it can be any from the levels you choose on the list. Even levels that you've finished already.</p>";
+   else if (s.id === "downloadformat")
+      document.getElementById("descriptionbox").innerHTML = "<h2>File Format</h2><p>You can choose between 3 file formats:</p><h4>GCT</h4><p>Download a GCT file for use with Nintendont</p><h4>Dolphin INI</h4><p>Download a textfile containing the formatted codes for use with Dolphin. Copy the contents of the file on top of your games .ini file.</p><p>You can open the .ini file by right clicking the game in Dolphin. In the context menu select 'Properties' and then 'Edit configuration'.</p><h4>Cheat Manager TXT</h4><p>Download the cheats in a textfile formatted for use with the <a target=\"_blank\" href=\"http://wiibrew.org/wiki/CheatManager\">Gecko Cheat Manager</a>. Place the txt file in SD:/txtcodes/.</p><p>A zip archive containing pregenerated txt files with all available codes on this site can be downloaded <a target=\"_blank\" href=\"files/GCMCodes.zip\">here</a>.</p>";
+   else if (s.id === "stageloader")
+      document.getElementById("descriptionbox").innerHTML = "<h2>Stage Loader</h2><p>Select yes if you want to use a custom stage loader, which automatically loads the levels you choose, similiar to 'Fast Any%'.</p>";
+   else if (s.id === "gameversion")
+      document.getElementById("descriptionbox").innerHTML = "<h2>Game Version</h2><p>Select your game version here. NTSC-U stands for the North American version of the game, PAL for the European and NTSC-J 1.0 and 1.1 for the two Japanese releases.</p>"
 }
 
 function resetDescription() {
-   document.getElementById("descriptionbox").innerHTML = "<p><h3>Select your codes from the list...</h3></p>";
+   document.getElementById("descriptionbox").innerHTML = "<p><h3>Choose your codes from the list...</h3></p>";
+}
+
+function switchPosition() {
+   document.getElementById("ph_gameversion").appendChild(document.getElementById("gameversion"));
+   document.getElementById("gameversion").setAttribute("onchange","updateCodelist()");
+   document.getElementById("gameversion").style.width = "100%";
+   updateCodelist();
 }
 
 function updateChangelog() {
+   document.getElementById("gameversion").style.visibility = "visible";
    var xml = new XMLHttpRequest();
    var file = "changelog.xml";
    xml.onload = function() {
@@ -205,6 +218,7 @@ function updateChangelog() {
          changelogData = (new DOMParser()).parseFromString(xml.responseText, "text/xml");
          changelogData = changelogData.getElementsByTagName("update");
 
+         document.getElementById("lastupdate").innerHTML = "Last Updated: " + changelogData[0].getElementsByTagName("date")[0].textContent;
          for (var i = 0; i < changelogData.length && i < 5; i++) {
             document.getElementById("changelog").innerHTML += "<p style=\"margin:0\"><i>" + changelogData[i].getElementsByTagName("date")[0].textContent + ":</i> " + changelogData[i].getElementsByTagName("change")[0].textContent + "</p>";
          }
@@ -268,35 +282,35 @@ document.querySelector("#route_clear").addEventListener("click", function () {
 
 {
    let selection;
-   
+
    function selDragStart(e) {
       selection = this;
       e.dataTransfer.effectAllowed = "move";
       e.dataTransfer.setData("text/html", this.querySelector("select").value);
-      
+
       this.classList.add("dragelement");
    }
-   
+
    function selDragOver(e) {
       if (e.preventDefault) {
          e.preventDefault();
       }
-      
+
       this.classList.add("dragover");
       e.dataTransfer.dropEffect = "move";
-      
+
       return false;
    }
-   
+
    function selDragLeave() {
       this.classList.remove("dragover");
    }
-   
+
    function selDragDrop(e) {
       if (e.stopPropagation) {
          e.stopPropagation();
       }
-      
+
       if (selection != this) {
          this.parentNode.removeChild(selection);
          this.insertAdjacentHTML("afterend", template.outerHTML);
@@ -304,15 +318,15 @@ document.querySelector("#route_clear").addEventListener("click", function () {
          this.nextSibling.draggable = true;
          selSetHandlers(this.nextSibling);
       }
-      
+
       this.classList.remove("dragover");
       return false;
    }
-   
+
    function selDragEnd() {
       this.classList.remove("dragelement");
    }
-   
+
    function selSetHandlers(elem) {
       elem.addEventListener('dragstart', selDragStart, false);
       elem.addEventListener('dragover', selDragOver, false);
@@ -322,15 +336,15 @@ document.querySelector("#route_clear").addEventListener("click", function () {
    }
 }
 
-//Interface 
+//Interface
 function getFastCode() {
-   
+
    const levelCodes = Array.prototype.map.call(levels.querySelectorAll("select"), s => s.value);
-   
+
    levelCodes.pop();
-   
-   if (!(document.getElementById("usefastcode").checked) || levelCodes.length === 1) return false;  
-   
+
+   if (!(document.getElementById("usefastcode").checked) || levelCodes.length === 1) return false;
+
    let game = JSON.parse(atob(document.getElementById("route_levels").getAttribute("data-json")));
    const randomize = (document.getElementById("route_random").value === "yes");
    const levelWords = Math.ceil(levelCodes.length / 2);
@@ -346,9 +360,9 @@ function getFastCode() {
    asm.push("9004010C"); // stw r0, 0x010C(r4)
    asm.push("38000001"); // li r0, 1
    asm.push("98040101"); // stb r0, 0x0101(r4)
-   
+
    asm.push("881F0012"); // lbz r0, 0x12(r31)
-   
+
    if (!randomize) {
       asm.push("2C00000F"); // cmpwi r0, 15
       asm.push("40820010"); // bne- 0x10
@@ -356,9 +370,9 @@ function getFastCode() {
       asm.push("90040000"); // stw r0, 0(r4)
       asm.push("4800002C"); // b 0x2C
    }
-   
+
    asm.push("2C000001"); // cmpwi r0, 1
-   
+
    if (randomize) {
       asm.push("41810030"); // bgt- 0x30
       asm.push("7C6C42E6"); // mftbl r3
@@ -373,28 +387,28 @@ function getFastCode() {
       asm.push("38030002"); // addi r0, r3, 2
       asm.push("90040000"); // stw r0, 0(r4)
    }
-   
+
    asm.push("7C8802A6"); // mflr r4
    asm.push("7C641A2E"); // lhzx r3, r4, r3
    asm.push("B07F0012"); // sth r3, 0x12(r31)
    asm.push("808D" + game.fmOffset); // lwz r4, TFlagManager::smInstance
    asm.push("986400DF"); // stb r3, 0xDF(r4)
    asm.push("807F0020"); // lwz r3, 0x20(r31)
-   
+
    if (asm.length % 2 === 0) {
       asm.push("60000000"); // nop
    }
    asm.push("00000000");
-   
+
    const geckoLines = asm.length / 2;
    let gecko = "C2" + game.injectAddr + " " + ("0000000" + geckoLines.toString(16).toUpperCase()).slice(-8) + "\r\n";
    for (let i = 0; i < geckoLines; ++i) {
       gecko += asm[2 * i] + " " + asm[2 * i + 1] + "\r\n";
    }
-   
-   let codes = gecko + 
+
+   let codes = gecko +
       game.notext[document.getElementById("route_notext").value] +
       game.nofmvs[document.getElementById("route_nofmvs").value];
-   
+
    return codes.replace(/[^0-9A-F]/g, '');
 }
